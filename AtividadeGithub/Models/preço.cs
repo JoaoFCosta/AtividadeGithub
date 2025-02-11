cf25da91ef34c26a8c6629065f789d7b351266e6
Livia Solla <limasenai12@gmail.com>
2025-02-11 16:29:22 -0300

    Criando a Models

--- AtividadeGithub/Models/preço.cs
+++ AtividadeGithub/Models/preço.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public string NomeNo { get; set; }
+        public string NomeEn { get; set; }
     }
 }
